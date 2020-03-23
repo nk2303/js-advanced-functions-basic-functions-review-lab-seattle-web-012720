10b1689a97d6fb34e605d96d99a3e5d3b280ad20
github username <github email address>
2020-03-22 22:39:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function saturdayFun(act = "roller-skate"){
+    return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(str = "go to the office"){
+    return `This Monday, I will ${str}.`;
+}
+
+function wrapAdjective(str = "*"){
+    return (param = "special") => {
+        return `You are ${str}${param}${str}!`
+    };
+}
+
+const Calculator = {
+    add : function add() {return 1+3},
+    subtract : function subtract() {return 1-3},
+    multiply : function multiply() {return 1*3},
+    divide : function divide() {return 10/5}
+}
+
+function actionApplyer(int, arr){
+    return arr.reduce((acc, func) => func(acc), int);
+}
